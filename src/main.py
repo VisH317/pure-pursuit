c319ddcc093a737b6787cd2057da55fa5d243101
VisH317 <54584201+VisH317@users.noreply.github.com>
2023-05-27 17:53:26 -0400

    rectangle is setup

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 import pygame
+from pygame import Rect
+import colors
+from points import Point
+from typing import List
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((600, 600))
+screen.fill(colors.SCREEN)
+pygame.display.update()
+
+rect = Rect(275, 275, 50, 50)
+
+pygame.draw.rect(screen, colors.BOT, rect)
+
+point_list: List[Point] = []
 
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT: running = False
+
+    pygame.display.flip()
 
 pygame.quit()
