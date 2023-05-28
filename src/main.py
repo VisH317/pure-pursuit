a02642e3f32d39273e30ef18c9e477de71ea899f
VisH317 <54584201+VisH317@users.noreply.github.com>
2023-05-27 23:09:07 -0400

    changes have been made pog

--- src/main.py
+++ src/main.py
@@ -4,17 +4,34 @@ import colors
 from points import Point
 from typing import List
 
+# CONFIG VARS
+RADIUS = 50
+
+
+
 pygame.init()
 
 screen = pygame.display.set_mode((600, 600))
 screen.fill(colors.SCREEN)
 pygame.display.update()
 
-rect = Rect(275, 275, 50, 50)
+rect = Rect(290, 290, 20, 20)
 
 pygame.draw.rect(screen, colors.BOT, rect)
 
-point_list: List[Point] = []
+# state functions
+
+def getHeading():
+    rect.
+
+
+point_list: List[Point] = [Point(300, 300), Point(100, 300), Point(100, 100), Point(300, 100), Point(300, 300)]
+
+for ix,point in enumerate(point_list):
+    if ix==len(point_list)-1: continue
+    pygame.draw.line(screen, colors.LINE, point, point_list[ix+1])
+
+# pygame.draw.circle(screen, colors.LINE, rect.center, RADIUS, width=0)
 
 running = True
 
